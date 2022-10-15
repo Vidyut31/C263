d25ec04b7d2fb87a290d145fea973d3fa381e3dc
Vidyut31 <vidyut2738@gmail.com>
2022-10-15 07:20:53 +0530

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
+#this decorator tells the computer to execute the program in an internet browser of the computer
 def visitors():
 
     # Load current count
@@ -21,7 +22,8 @@ def visitors():
     # Render HTML with count variable
     return render_template("index.html", count=visitors_count)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST']) #POST method will help us to fetch data from the input field.
+
 def covid_stats():
     # Load current count
     counter_read_file = open("count.txt", "r")
@@ -29,10 +31,17 @@ def covid_stats():
     counter_read_file.close()
 
     text = request.form['text']
+    """ the request is a function imported from the flask library. It
+    works as a middle man between Python code and HTML code."""
 
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
+    corona_data = 'https://covid-api-262.herokuapp.com/?country='+text
     print(corona_data)
     return render_template("index.html", image=corona_data, count=visitors_count)
 
 if __name__ == "__main__":
     app.run()
+"""we will check if still, we are under flask code by writing the if
+condition and if the condition satisfies then we will run our flask application by
+using run() function followed by the variable app which holds the initialization
+of flask.
+"""
